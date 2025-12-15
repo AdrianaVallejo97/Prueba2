938e3177d67f2a2722022e31eb662db9ac507ae2
Adriana Vallejo <atvallejomunoz@hotmail.com>
2025-12-15 13:53:08 -0500

    update

--- app/backend/index.js
+++ app/backend/index.js
@@ -3,11 +3,9 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/api") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Hola desde el Backend 🚀" }));
-  } else {
-    res.writeHead(404);
-    res.end();
+    res.end(JSON.stringify({ message: "Hola desde el Backend" }));
   }
 });
 
 server.listen(3000);
+
